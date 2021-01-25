0140ad78f30af2ed050ca24d75208d32722176f0
li_zhang_yu <815937345@qq.com>
2021-01-25 17:12:41 +0800

    lizhangyu_学习cas 、 getway

--- src/main/java/org/example/controller/HelloWorld.java
+++ src/main/java/org/example/controller/HelloWorld.java
@@ -3,10 +3,17 @@ package org.example.controller;
 import org.example.service.TestService;
 import org.example.utils.SpringContextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,14 +21,30 @@ import java.util.Map;
 public class HelloWorld {
     @Autowired
     private TestService testService;
+
+    private static FileOutputStream fileOutputStream;
+
+    static {
+        try {
+            fileOutputStream = new FileOutputStream(new File("/logs/a.txt"));
+        }catch (Exception e){
+
+        }
+    }
+
   @RequestMapping("api/hello")
-  Map<String,String> hello() {
+  Map<String,String> hello() throws Exception {
       Map<String,String> map = new HashMap<>();
-      map.put("result","create by 迟墨！");
+      map.put("result","create by lizhangyu！");
+
+      fileOutputStream.write("hhhh\n".getBytes());
+      fileOutputStream.flush();
+
       return map;
   }
     @RequestMapping("asyncMath1")
-    Map<String,String> asyncMath1() {
+    Map<String,String> asyncMath1(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
         System.out.println("调用service asyncMath1开始");
         ThreadPoolTaskExecutor threadPoolTaskExecutor = (ThreadPoolTaskExecutor)SpringContextUtils.getBean("name1");
         System.out.println("当前活动线程数：" + threadPoolTaskExecutor.getActiveCount());
@@ -32,7 +55,9 @@ public class HelloWorld {
         return map;
     }
     @RequestMapping("asyncMath2")
-    Map<String,String> asyncMath2() {
+    Map<String,String> asyncMath2(HttpServletRequest request) {
+
+        System.out.println(""+request.getHeader("X-Request-Foo"));
         System.out.println("调用service asyncMath2开始");
         testService.asyncMath2();
         System.out.println("调用service asyncMath2结束");
@@ -46,7 +71,12 @@ public class HelloWorld {
         testService.asyncMath3();
         System.out.println("调用service asyncMath3结束");
         Map<String,String> map = new HashMap<>();
-        map.put("result","asyncMath2");
+        map.put("result","asyncMath3");
         return map;
+    }
+
+    @PreDestroy
+    public void destroyMethod() throws Exception {
+        fileOutputStream.close();
     }
 }
